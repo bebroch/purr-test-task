617356f45cd8b6db5022327c0be4c9f73fe718af
bebroch <cawapog@yandex.ru>
2024-05-21 19:13:57 +0300

    init: column, card, comment services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { DatabaseModule } from './database/database.module'
+import { ColumnModule } from './column/column.module'
+import { CardModule } from './card/card.module'
+import { CommentModule } from './comment/comment.module'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         DatabaseModule,
         AuthModule,
+        ColumnModule,
+        CardModule,
+        CommentModule,
     ],
 })
 export class AppModule {}

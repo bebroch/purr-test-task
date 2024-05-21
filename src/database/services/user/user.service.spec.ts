6d0ee56a004ff33995182fe7886fbc3e407d90b2
bebroch <cawapog@yandex.ru>
2024-05-21 15:25:30 +0300

    feat: test for user service

--- src/database/services/user/user.service.spec.ts
+++ src/database/services/user/user.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { MockType } from 'test/types/mock.type'
+import { Repository } from 'typeorm'
+import { UserEntity } from '../../entities/user.entity'
+import { CreateUserDto } from './dto/create-user.dto'
 import { UserService } from './user.service'
 
 describe('UserService', () => {
     let service: UserService
+    let repositoryMock: MockType<Repository<UserEntity>>
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UserService],
+            providers: [
+                UserService,
+                {
+                    provide: getRepositoryToken(UserEntity),
+                    useFactory: jest.fn(() => ({
+                        create: jest.fn((entity) => entity),
+                        save: jest.fn((entity) => entity),
+                    })),
+                },
+            ],
         }).compile()
 
         service = module.get<UserService>(UserService)
+        repositoryMock = module.get(getRepositoryToken(UserEntity))
     })
 
-    it('should be defined', () => {
-        expect(service).toBeDefined()
+    it('test create', async () => {
+        const user = { email: 'Alni', password: 'password', id: 1 }
+        const userDto = new CreateUserDto({
+            email: 'Alni',
+            password: 'password',
+        })
+        repositoryMock.create.mockReturnValue(user)
+        expect(await service.create(userDto)).toEqual(user)
+        expect(repositoryMock.create).toHaveBeenCalledWith(userDto.get())
     })
 })

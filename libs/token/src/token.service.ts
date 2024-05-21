d1758782f8dae848b118d0caaf76d60d16449337
bebroch <cawapog@yandex.ru>
2024-05-21 19:08:14 +0300

    feat: token service

---
+++ libs/token/src/token.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+
+@Injectable()
+export class TokenService {
+    constructor(private readonly jwtService: JwtService) {}
+
+    async sign(payload: Record<string, unknown>) {
+        return await this.jwtService.signAsync(payload)
+    }
+
+    async decode(token: string) {
+        return await this.jwtService.decode(token)
+    }
+}
